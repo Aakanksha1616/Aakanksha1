3d1918955787979df93de70c1e0b5b77ab588a96
aakanksharaut <110025500+aakanksharaut@users.noreply.github.com>
2022-12-06 10:44:13 +0530

    puja

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './component/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
